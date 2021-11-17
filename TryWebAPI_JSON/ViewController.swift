c956ebbc0e2cccebd7bdc0d755ac2936446a7d10
fumi2mi <imafumi.murataka@gmail.com>
2021-11-17 11:09:43 +0900

    Initial Commit

---
+++ TryWebAPI_JSON/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TryWebAPI_JSON
+//
+//  Created by Fumitaka Imamura on 2021/11/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
