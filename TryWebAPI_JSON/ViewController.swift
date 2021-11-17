17ed38ed1f5264413cb1cdbbf471690b76d681fe
fumi2mi <imafumi.murataka@gmail.com>
2021-11-17 11:46:56 +0900

    RequestURLを作成

--- TryWebAPI_JSON/ViewController.swift
+++ TryWebAPI_JSON/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UISearchBarDelegate {
+class ViewController: UIViewController {
 
     @IBOutlet weak private var searchText: UISearchBar!
     @IBOutlet weak private var tableView: UITableView!
@@ -17,5 +17,24 @@ class ViewController: UIViewController, UISearchBarDelegate {
         searchText.delegate = self
         searchText.placeholder = "お菓子の名前を入力してください"
     }
+}
+
+extension ViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        view.endEditing(true)
+        if let searchWord = searchBar.text {
+            print(searchWord)
+            searchOkashi(keyword: searchWord)
+        }
+    }
+
+    private func searchOkashi(keyword: String) {
+        guard let keyword_encode = keyword.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+        else { return }
+        guard let req_url = URL(string: "https://sysbird.jp/toriko/api/?apikey=guest&format=json&keyword=\(keyword_encode)&max=10&order=r")
+        else { return }
+        print(req_url)
+    }
+
 }
 
